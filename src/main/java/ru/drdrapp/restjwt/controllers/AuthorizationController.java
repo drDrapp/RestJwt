60570e6d3a9437e4e766dfe170905a074f0f36c4
drDrapp <born--in--ussr@mail.ru>
2023-05-26 11:01:48 +0300

    Fix auth by role

--- src/main/java/ru/drdrapp/restjwt/controllers/AuthorizationController.java
+++ src/main/java/ru/drdrapp/restjwt/controllers/AuthorizationController.java
@@ -1,6 +1,5 @@
 package ru.drdrapp.restjwt.controllers;
 
-import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;

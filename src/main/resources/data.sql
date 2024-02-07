d1a040f0865a9b3746041acf74d28bb00300c6db
drDrapp <born--in--ussr@mail.ru>
2024-02-07 10:01:12 +0300

    Add http request for admin data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,9 @@
-INSERT INTO dg_user (id, first_Name, last_Name, login, hash_Password, state)
+INSERT INTO dg_user (id, first_Name, last_Name, login, hash_password, state)
 VALUES ('1', 'firstName', 'lastName', 'z', '$2a$10$qz7.2I91D3Ea6zXvkxjw1.EdY2m7jAKSxlLePLeo3LDpxED6MY6Me', 'ACTIVE');
 INSERT INTO dg_user_role
 VALUES ('1', 'ADMIN');
+
+INSERT INTO dg_user (id, first_Name, last_Name, login, hash_password, state)
+VALUES ('2', 'userFirstName', 'userLastName', 'u', '$2a$10$qz7.2I91D3Ea6zXvkxjw1.EdY2m7jAKSxlLePLeo3LDpxED6MY6Me', 'ACTIVE');
+INSERT INTO dg_user_role
+VALUES ('2', 'USER');
